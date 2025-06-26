52b4aa88b2469d5e3b4dd25d955b5efbcce76bbb
mMustajabAhmad <mustajabajwah@gmail.com>
2025-06-26 12:40:00 +0500

    driver-task: setup task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  include ActiveModel::Dirty
+
   belongs_to :recipient
   has_one :address, as: :addressable, dependent: :destroy
   has_one :feedback, dependent: :destroy
@@ -17,11 +19,12 @@ class Task < ApplicationRecord
   accepts_nested_attributes_for :recipient
   accepts_nested_attributes_for :address
 
-  enum :state, [:unassigned, :assigned, :active, :completed]
+  enum :state, [:unassigned, :assigned, :active, :completed, :failed]
 
   validates :recipient, presence: true
   validates :address, presence: true
   validates :pickup_task, inclusion: {in: [true, false]}
+  validate :valid_state_transition
 
   before_create :generate_short_id
   after_initialize :set_default_state, if: :new_record?
@@ -53,5 +56,13 @@ class Task < ApplicationRecord
 
     def set_default_state
       self.state ||= :unassigned
+    end
+
+    def valid_state_transition
+      if state_changed?
+        if (state == "completed" || state == "failed") && state_was != "active"
+          errors.add(:state, "must be active before it can be completed or failed")
+        end
+      end
     end
 end
