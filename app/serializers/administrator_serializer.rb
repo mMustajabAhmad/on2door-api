8baab0cf8d0751dac77c121d0c9e97d89534d927
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-08 10:21:13 +0500

    admins_teams_setup: fix serilizers attributes

--- app/serializers/administrator_serializer.rb
+++ app/serializers/administrator_serializer.rb
@@ -1,16 +1,13 @@
 class AdministratorSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :email, :first_name, :last_name, :phone_number, :role, :organization_id, :is_active, :is_account_owner, :invited_by_id, :invited_by_type, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at
+  attributes :id, :email, :first_name, :last_name, :phone_number, :role, :organization_id, :pending_team_ids, :is_active, :is_account_owner, :is_read_only, :invited_by_id, :invited_by_type, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at
 
-  attribute :invited_by_role do |object|
-    if object.invited_by_id.present?
-      inviter = Administrator.find_by(id: object.invited_by_id)
-      inviter&.role
-    end
+  attribute :invited_by_role, if: Proc.new { |administrator| administrator.invited_by_id.present? } do |administrator|
+    Administrator.find_by(id: administrator.invited_by_id)&.role
   end
 
-  attribute :teams do |object|
+  attribute :team_ids do |object|
     object.team_ids
   end
 end
