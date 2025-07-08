a802fec95ccd79c84d953234e848871e1dd0c7cc
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-08 10:21:13 +0500

    admins_teams_setup: fix serilizers attributes

--- app/serializers/driver_serializer.rb
+++ app/serializers/driver_serializer.rb
@@ -3,10 +3,7 @@ class DriverSerializer
 
   attributes :id, :email, :first_name, :last_name, :is_active, :organization_id,  :invited_by_id, :invited_by_type, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at
 
-  attribute :invited_by_role do |object|
-    if object.invited_by_id.present?
-      inviter = Administrator.find_by(id: object.invited_by_id)
-      inviter&.role
-    end
+  attribute :invited_by_role, if: Proc.new { |driver| driver.invited_by_id.present? } do |driver|
+    Administrator.find_by(id: driver.invited_by_id)&.role
   end
 end
--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,9 +1,13 @@
 class TeamSerializer
   include JSONAPI::Serializer
-  
-  attributes :id, :name, :enable_self_assign, :organization_id, :hub_id
 
-  attribute :administrator_ids do |object|
-    object.administrator_ids
+  attributes :id, :name, :organization_id, :hub_id
+
+  attribute :dispatcher_ids do |object|
+    object.administrators.dispatcher.ids
+  end
+
+  attribute :dispatchers_count do |object|
+    object.administrators.dispatcher.count
   end
 end
