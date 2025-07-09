44c73c0c0dba2e40ac819fd7101c1bd36788ae61
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-09 23:41:31 +0500

    admin_teams_setup: add team ids while inviting dispatcher

--- app/controllers/api/v1/administrators/invitations_controller.rb
+++ app/controllers/api/v1/administrators/invitations_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::Administrators::InvitationsController < Devise::InvitationsContro
 
   def create
     if params[:role] == 'owner'
-      render json: { 
+      render json: {
         error: "You cannot invite an owner."
       }, status: :unprocessable_entity
       return
     end
-    
+
     admin = Administrator.invite!(
       {
         email: params[:email],
@@ -17,10 +17,11 @@ class Api::V1::Administrators::InvitationsController < Devise::InvitationsContro
         last_name: params[:last_name],
         phone_number: params[:phone_number],
         role: params[:role],
-        is_read_only: params[:is_read_only],
+        is_read_only: params[:role] == 'dispatcher' ? params[:is_read_only] : false,
         is_account_owner: false,
         is_active: false,
-        organization_id: current_administrator.organization_id
+        organization_id: current_administrator.organization_id,
+        pending_team_ids: params[:role] == 'dispatcher' ? params[:team_ids]&.map(&:to_i) : nil
       },
       current_administrator
     )
@@ -42,6 +43,13 @@ class Api::V1::Administrators::InvitationsController < Devise::InvitationsContro
       :password,
       :password_confirmation
     ))
+
+    if admin.role == 'dispatcher' && admin.pending_team_ids.present?
+      teams = Team.where(id: admin.pending_team_ids, organization_id: admin.organization_id)
+      admin.teams = teams
+      admin.pending_team_ids = nil
+      admin.save
+    end
 
     if admin.valid?
       render json: {
