354b4d857ce0269baf831ad59265b9a054a3f670
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-13 21:43:43 +0500

    admin_teams_setup: add methods for organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,25 +3,33 @@ class Organization < ApplicationRecord
   has_many :drivers, dependent: :destroy
   has_many :teams, dependent: :destroy
 
-  enum monthly_delivery_volume: {
-    range_0_100: 0,
-    range_101_2000: 1,
-    range_2001_5000: 2,
-    range_5001_12500: 3,
-    range_12501_plus: 4
-  }
+  enum :monthly_delivery_volume, [
+    :range_0_100,
+    :range_101_2000,
+    :range_2001_5000,
+    :range_5001_12500,
+    :range_12501_plus
+  ]
 
-  enum primary_industry: {
-    construction: 0, 
-    grocery: 1, 
-    pharmacy: 2, 
-    preparedfood: 3, 
-    restaurants: 4, 
-    retail: 5, 
-    others: 6
-  }
+  enum :primary_industry, [
+    :construction,
+    :grocery,
+    :pharmacy,
+    :preparedfood,
+    :restaurants,
+    :retail,
+    :others
+  ]
 
-  def org_owner
+  def owner
     administrators.owner
+  end
+
+  def admins
+    administrators.admin
+  end
+
+  def dispatchers
+    administrators.dispatcher
   end
 end
