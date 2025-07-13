f024776c9fa87614fa297716237bb4790d02eed4
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-13 21:44:39 +0500

    admins_teams_setup: establish association between drivers and teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,8 @@ class Team < ApplicationRecord
   belongs_to :organization
   belongs_to :hub, optional: true
 
-  has_and_belongs_to_many :drivers, join_table: :drivers_teams
   has_and_belongs_to_many :administrators, join_table: :administrators_teams
+  has_and_belongs_to_many :drivers, join_table: :drivers_teams
 
   validates :name, presence: true
 end
