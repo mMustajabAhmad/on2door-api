372cad6d88682c89e1a4e8e0ade0ba96407759fe
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-13 21:39:41 +0500

    admin_teams_setup: add crud and searching for administrators

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -6,11 +6,15 @@ class Administrator < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   belongs_to :organization
+  accepts_nested_attributes_for :organization
   after_invitation_accepted :activate_admin
-  has_and_belongs_to_many :teams
-  validates :is_account_owner, uniqueness: { scope: :organization_id }, if: :is_account_owner?
+  has_and_belongs_to_many :teams, join_table: :administrators_teams
 
   enum :role, [ :owner, :admin, :dispatcher ]
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "email", "first_name", "last_name", "invitation_accepted_at", "is_account_owner", "is_active", "is_read_only", "organization_id", "phone_number", "role"]
+  end
 
   def activate_admin
     update(is_active: true)
