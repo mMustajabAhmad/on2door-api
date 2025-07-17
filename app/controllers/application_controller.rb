2baa0445b7374e33560ab0db8a42c54a64bea85b
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-17 16:26:22 +0500

    admins_teams_setup: fix cancan working by definig current ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include Pagy::Backend
+  include CanCan::ControllerAdditions
+
+  def current_ability
+    @current_ability ||= Ability.new(current_administrator || current_driver)
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { error: "Access denied: #{exception.message}" }, status: :forbidden
+  end
 end
