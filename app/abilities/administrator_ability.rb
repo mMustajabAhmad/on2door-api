6dd2113b8135c55c735fb4b737508958b1fe231f
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-17 16:31:30 +0500

    admins_team_setup: add abilities on organization hub team

--- app/abilities/administrator_ability.rb
+++ app/abilities/administrator_ability.rb
@@ -4,6 +4,9 @@ class AdministratorAbility
       ability.can :manage, Administrator, organization_id: user.organization_id
       ability.cannot :destroy, Administrator, id: user.id
       ability.can :create, Administrator, role: ['admin', 'dispatcher']
+      ability.can :manage, Organization, id: user.organization_id
+      ability.can :manage, Team, organization_id: user.organization_id
+      ability.can :manage, Hub, organization_id: user.organization_id
     elsif user.admin?
       ability.can :read, Administrator, organization_id: user.organization_id
       ability.can :create, Administrator, role: ['admin', 'dispatcher']
@@ -11,9 +14,13 @@ class AdministratorAbility
       ability.can :update, Administrator, organization_id: user.organization_id, role: ['admin', 'dispatcher']
       ability.can :destroy, Administrator, organization_id: user.organization_id, role: ['admin', 'dispatcher']
       ability.cannot :destroy, Administrator, id: user.id
+      ability.can :manage, Organization, id: user.organization_id
+      ability.can :manage, Team, organization_id: user.organization_id
+      ability.can :manage, Hub, organization_id: user.organization_id
     elsif user.dispatcher?
       ability.can :index, Administrator, role: 'dispatcher', teams: { id: user.team_ids }
       ability.can :update, Administrator, id: user.id
+      ability.can :read, Team, id: user.team_ids
     else
       ability.cannot :manage, :all
     end
