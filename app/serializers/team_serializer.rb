45b6bf2aa16dcefd6845e831c869eb3a3a8bb021
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-18 00:20:29 +0500

    admin_team_setup: fix team crud

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,13 +1,5 @@
 class TeamSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :name, :organization_id, :hub_id
-
-  attribute :dispatcher_ids do |object|
-    object.administrators.dispatcher.ids
-  end
-
-  attribute :dispatchers_count do |object|
-    object.administrators.dispatcher.count
-  end
+  attributes :id, :name, :organization_id, :hub_id, :dispatchers_count
 end
