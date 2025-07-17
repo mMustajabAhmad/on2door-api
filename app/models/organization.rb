8604e100754e2ab264732cfcefc9eebca4e01189
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-17 16:27:41 +0500

    admins_teams_setup: fix timezone working

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,14 @@
 class Organization < ApplicationRecord
+  include CountryTimezone
   has_many :administrators, dependent: :destroy
   has_many :drivers, dependent: :destroy
   has_many :teams, dependent: :destroy
+  has_many :hubs, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  before_validation :set_timezone_from_country
+
+  delegate :owner, :admin, :dispatcher, to: :administrators
 
   enum :monthly_delivery_volume, [
     :range_0_100,
@@ -21,15 +28,14 @@ class Organization < ApplicationRecord
     :others
   ]
 
-  def owner
-    administrators.owner
-  end
-
-  def admins
-    administrators.admin
-  end
-
-  def dispatchers
-    administrators.dispatcher
-  end
+  private
+    def set_timezone_from_country
+      return if country.blank?
+      tz = timezone_for_country(country)
+      if tz.nil?
+        errors.add(:country, "is invalid. Please use the official English country name or 2-letter code.")
+      else
+        self.timezone = tz
+      end
+    end
 end
