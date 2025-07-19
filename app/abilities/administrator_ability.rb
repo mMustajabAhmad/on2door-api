c1970dd1f78eea28de97c16e0e55adb49f8e0497
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-19 20:26:08 +0500

    admins_teams_setup: update cancan abilities

--- app/abilities/administrator_ability.rb
+++ app/abilities/administrator_ability.rb
@@ -1,22 +1,22 @@
 class AdministratorAbility
   def initialize(user, ability)
     if user.owner?
-      ability.can :manage, Administrator, organization_id: user.organization_id
+      ability.can :manage, Administrator
       ability.cannot :destroy, Administrator, id: user.id
       ability.can :create, Administrator, role: ['admin', 'dispatcher']
-      ability.can :manage, Organization, id: user.organization_id
-      ability.can :manage, Team, organization_id: user.organization_id
-      ability.can :manage, Hub, organization_id: user.organization_id
+      ability.can :manage, Organization
+      ability.can :manage, Team
+      ability.can :manage, Hub
     elsif user.admin?
-      ability.can :read, Administrator, organization_id: user.organization_id
+      ability.can :read, Administrator
       ability.can :create, Administrator, role: ['admin', 'dispatcher']
       ability.cannot :show, Administrator, role: 'owner'
-      ability.can :update, Administrator, organization_id: user.organization_id, role: ['admin', 'dispatcher']
-      ability.can :destroy, Administrator, organization_id: user.organization_id, role: ['admin', 'dispatcher']
+      ability.can :update, Administrator, role: ['admin', 'dispatcher']
+      ability.can :destroy, Administrator, role: ['admin', 'dispatcher']
       ability.cannot :destroy, Administrator, id: user.id
-      ability.can :manage, Organization, id: user.organization_id
-      ability.can :manage, Team, organization_id: user.organization_id
-      ability.can :manage, Hub, organization_id: user.organization_id
+      ability.can :manage, Organization
+      ability.can :manage, Team
+      ability.can :manage, Hub
     elsif user.dispatcher?
       ability.can :index, Administrator, role: 'dispatcher', teams: { id: user.team_ids }
       ability.can :update, Administrator, id: user.id
