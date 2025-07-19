d5e8148bc7336773b8f6df807af142dbce39a905
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-20 00:23:21 +0500

    driver: add driver crud operations

---
+++ app/controllers/api/v1/administrators/drivers_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::Administrators::DriversController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    q = @drivers.ransack(params[:q])
+    pagy, records = pagy(q.result, page: params[:page],limit: params[:per_page])
+
+    render json: { drivers: DriverSerializer.new(records).as_json, total_count: pagy.count}, status: :ok
+  end
+
+  def show
+    render json: { driver: DriverSerializer.new(@driver).as_json }, status: :ok
+  end
+
+  def update
+    return render json: { error: 'Driver must be assigned to at least one team'}, status: :unprocessable_entity if driver_params[:team_ids].blank?
+
+    if @driver.update(driver_params)
+      render json: { driver: DriverSerializer.new(@driver).as_json }, status: :ok
+    else
+      render json: { error: driver.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @driver.destroy
+      render json: { message: 'Driver deleted successfully' }, status: :ok
+    else
+      render json: { error: driver.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def driver_params
+      params.require(:driver).permit(:email, :first_name, :last_name, :phone_number, team_ids: [])
+    end
+end
