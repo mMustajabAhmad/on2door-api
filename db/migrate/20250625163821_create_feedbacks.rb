6189fa37d0db3e04e07d807983789df07015c750
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-22 13:02:36 +0500

    task: fix task and related migrations

--- db/migrate/20250625163821_create_feedbacks.rb
+++ db/migrate/20250625163821_create_feedbacks.rb
@@ -1,10 +1,15 @@
 class CreateFeedbacks < ActiveRecord::Migration[7.2]
-  def change
+  def up
     create_table :feedbacks do |t|
       t.text :comment
-    
+
       t.timestamps
     end
+
     add_reference :feedbacks, :task, foreign_key: true
+  end
+
+  def down
+    drop_table :feedbacks
   end
 end
