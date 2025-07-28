cb0fc63c9d4abb0538257538be03d1f766ba28f7
mMustajabAhmad <mustajabajwah@gmail.com>
2025-07-28 12:40:00 +0500

    schedule: add controllers for admin to manage schdules

---
+++ app/controllers/api/v1/administrators/subschedules_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::Administrators::SubschedulesController < ApplicationController
+  before_action :authenticate_administrator!
+  load_and_authorize_resource
+
+  def index
+    q = @subschedules.includes(:schedule).ransack(params[:q])
+    pagy, records = pagy(q.result, page: params[:page], limit: params[:per_page])
+
+    render json: { subschedules: SUBSCHEDULE_SERIALIZER.new(records).as_json, total_count: pagy.count }, status: :ok
+  end
+
+  def show
+    render json: { subschedule: SUBSCHEDULE_SERIALIZER.new(@subschedule).as_json }, status: :ok
+  end
+
+  def create
+    schedule = Schedule.find(subschedule_params[:schedule_id])
+    @subschedule = schedule.subschedules.build(subschedule_params)
+
+    if @subschedule.save
+      render json: { subschedule: SUBSCHEDULE_SERIALIZER.new(@subschedule).as_json }, status: :ok
+    else
+      render json: { error: @subschedule.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @subschedule.update(subschedule_params)
+      render json: { subschedule: SUBSCHEDULE_SERIALIZER.new(@subschedule).as_json }, status: :ok
+    else
+      render json: { error: @subschedule.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @subschedule.destroy
+      render json: { message: 'SubSchedule deleted successfully' }, status: :ok
+    else
+      render json: { error: @subschedule.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def subschedule_params
+      params.require(:subschedule).permit(:shift_start, :shift_end, :schedule_id)
+    end
+end
