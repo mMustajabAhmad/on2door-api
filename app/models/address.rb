a2700d932a763f090845391d16a04a5a08d165c9
mMustajabAhmad <mustajabajwah@gmail.com>
2025-10-02 11:40:00 +0500

    hub-search: add ransackable attributes and associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
   validates :street, :city, :country, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[street city state postal_code country]
+  end
 end
