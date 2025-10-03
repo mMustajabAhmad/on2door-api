3d5771342ce74c3dfdec9dd65ecb788601d109c0
mMustajabAhmad <mustajabajwah@gmail.com>
2025-10-03 15:40:00 +0500

    feedback: add production url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.action_mailer.default_url_options = { host: 'https://onfleet.onrender.com', protocol: 'https' }
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
