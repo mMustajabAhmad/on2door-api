7ab2fbee02c7914ec94926078046db0c97e4a4b0
mMustajabAhmad <mustajabajwah@gmail.com>
2025-10-10 10:46:40 +0500

    track: geocode address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,22 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
 
-  validates :street, :city, :country, presence: true
+  validates :street, :city, :state, :country, presence: true
+  validates :latitude, :longitude, presence: true, on: :update
 
   def self.ransackable_attributes(auth_object = nil)
     %w[street city state postal_code country]
   end
+
+  geocoded_by :full_address
+  after_validation :geocode, if: :address_changed?
+
+  def full_address
+    [street, city, state, appartment, postal_code, country].compact.join(', ')
+  end
+
+  private
+    def address_changed?
+      street_changed? || street_number_changed? || appartment_changed? || city_changed? || state_changed? || postal_code_changed? || country_changed?
+    end
 end
