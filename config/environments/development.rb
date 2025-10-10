2dc81fbc8fb7c8342753067f849f876cb52bf318
mMustajabAhmad <mustajabajwah@gmail.com>
2025-10-10 10:44:08 +0500

    track: setup action cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,13 +38,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 1025
-  }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -70,12 +63,18 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  # Allow Action Cable access from any origin for development
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [/http:\/\/localhost.*/, /https:\/\/localhost.*/]
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.default_url_options = { host: ENV['HOST'], port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
 
   config.after_initialize do
     Bullet.enable = true
