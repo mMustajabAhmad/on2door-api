34126adbd79d2856c5e6227714defada4a19a4c4
ayemenn <aymenmehmood812@gmail.com>
2025-10-22 13:47:24 +0500

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
   # Action Cable configuration
-  config.action_cable.url = "wss://onfleet.onrender.com/cable"
+  config.action_cable.url = "wss://onfleet-utfr.onrender.com/cable"
   config.action_cable.allowed_request_origins = [
     'https://on2door-fe.vercel.app',   # frontend
-    'https://onfleet.onrender.com'      # backend domain (Render)
+    'https://onfleet-utfr.onrender.com'      # backend domain (Render)
   ]
 
 # Optional (recommended if you deploy via Render or use SSL)
