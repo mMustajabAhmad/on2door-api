80bd87c7cccee2055518ee953c8ceaf7dddb9e31
ayemenn <aymenmehmood812@gmail.com>
2025-10-24 15:11:47 +0500

    Updated mailer for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['HOST'], port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "localhost",
+    address: "127.0.0.1",
     port: 1025
   }
 
