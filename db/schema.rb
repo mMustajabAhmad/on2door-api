66486a86b4018754e65f0724787b99545fd78755
mMustajabAhmad <mustajabajwah@gmail.com>
2025-10-28 11:01:05 +0500

    task-mailer: add email to recipients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_12_081044) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_28_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -164,6 +164,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_12_081044) do
     t.boolean "skip_sms_notification"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.index ["email"], name: "index_recipients_on_email"
   end
 
   create_table "schedules", force: :cascade do |t|
