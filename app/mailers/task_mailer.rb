97527429d4264c7d4f9074c1c90f5d08c0b25a78
mMustajabAhmad <mustajabajwah@gmail.com>
2025-10-30 12:44:22 +0500

    action-mailer: update task mailer

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,7 +1,4 @@
-class TaskMailer < ActionMailer::Base
-  require 'sendgrid-ruby'
-  default from: 'on2door@gmail.com'
-
+class TaskMailer < ApplicationMailer
   def track_task(task)
     @task = task
     @recipient = task.recipient
@@ -18,42 +15,4 @@ class TaskMailer < ActionMailer::Base
       html_content: html_content
     )
   end
-
-  private
-    def send_sendgrid_email(to:, subject:, html_content:)
-      if Rails.env.production?
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_PASSWORD'])
-
-        data = {
-          personalizations: [
-            {
-              to: [{ email: to }],
-              subject: subject
-            }
-          ],
-          from: { email: 'on2door@gmail.com' },
-          content: [
-            {
-              type: 'text/html',
-              value: html_content
-            }
-          ]
-        }
-
-        begin
-          response = sg.client.mail._('send').post(request_body: data)
-          Rails.logger.info "SendGrid email sent: #{response.status_code}"
-        rescue => e
-          Rails.logger.error "SendGrid email failed: #{e.message}"
-        end
-      else
-        # development - Mailcatcher
-        mail(
-          to: to,
-          subject: subject,
-          body: html_content,
-          content_type: 'text/html'
-        )
-      end
-    end
 end
